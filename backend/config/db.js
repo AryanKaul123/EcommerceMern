136e8ffd2e2a8af5a627e1b51b45187657033af2
AryanKaul123 <160542508+AryanKaul123@users.noreply.github.com>
2024-09-07 13:11:05 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,12 @@ const mongoose = require("mongoose")
 
 async function connectDB(){
     try{
-        await mongoose.connect(process.env.MONGODB_URI)
+        await mongoose.connect(process.env.MONGODB_URI,{
+          
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 30000  // Increase timeout to 30 seconds
+    });
     }catch(err){
         console.log(err)
     }
