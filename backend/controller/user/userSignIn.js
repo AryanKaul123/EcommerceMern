868b91a8def7051e42f5c97a5b6e6d5ff8578de4
AryanKaul123 <160542508+AryanKaul123@users.noreply.github.com>
2024-09-07 14:52:35 +0530

    Update userSignIn.js

--- backend/controller/user/userSignIn.js
+++ backend/controller/user/userSignIn.js
@@ -1,65 +1,60 @@
-const bcrypt = require('bcryptjs')
-const userModel = require('../../models/userModel')
+const bcrypt = require('bcryptjs');
+const userModel = require('../../models/userModel');
 const jwt = require('jsonwebtoken');
 
-async function userSignInController(req,res){
-    try{
-        const { email , password} = req.body
+async function userSignInController(req, res) {
+    try {
+        const { email, password } = req.body;
 
-        if(!email){
-            throw new Error("Please provide email")
+        if (!email) {
+            throw new Error("Please provide email");
         }
-        if(!password){
-             throw new Error("Please provide password")
-        }
-
-        const user = await userModel.findOne({email})
-
-       if(!user){
-            throw new Error("User not found")
-       }
-
-       const checkPassword = await bcrypt.compare(password,user.password)
-
-       console.log("checkPassoword",checkPassword)
-
-       if(checkPassword){
-        const tokenData = {
-            _id : user._id,
-            email : user.email,
-        }
-        const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET_KEY, { expiresIn: 60 * 60 * 8 });
-
-        const tokenOption = {
-            httpOnly : true,
-            secure : true
+        if (!password) {
+            throw new Error("Please provide password");
         }
 
-        res.cookie("token",token,tokenOption).status(200).json({
-            message : "Login successfully",
-            data : token,
-            success : true,
-            error : false
-        })
+        const user = await userModel.findOne({ email });
 
-       }else{
-         throw new Error("Please check Password")
-       }
+        if (!user) {
+            throw new Error("User not found");
+        }
 
+        const checkPassword = await bcrypt.compare(password, user.password);
 
+        console.log("Password check result:", checkPassword);
 
+        if (checkPassword) {
+            const tokenData = {
+                _id: user._id,
+                email: user.email
+            };
+            const token = jwt.sign(tokenData, process.env.TOKEN_SECRET_KEY, { expiresIn: '8h' });
 
+            const tokenOptions = {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === 'production', // Secure only in production
+                sameSite: 'None' // Adjust as necessary
+            };
 
+            res.cookie("token", token, tokenOptions).status(200).json({
+                message: "Login successfully",
+                data: token,
+                success: true,
+                error: false
+            });
 
+        } else {
+            throw new Error("Invalid password");
+        }
 
-    }catch(err){
-        res.json({
-            message : err.message || err  ,
-            error : true,
-            success : false,
-        })
+    } catch (err) {
+        console.error("Sign-in error:", err);
+        res.status(400).json({
+            message: err.message || 'An error occurred',
+            error: true,
+            success: false
+        });
     }
-
 }
 
-module.exports = userSignInController
+module.exports = userSignInController;
