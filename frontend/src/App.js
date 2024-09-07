f02457f3cf4918f9ebef1cae36efc0145ecd2ba7
AryanKaul123 <160542508+AryanKaul123@users.noreply.github.com>
2024-09-07 12:23:59 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,18 +15,7 @@ function App() {
   const dispatch = useDispatch()
   const [cartProductCount,setCartProductCount] = useState(0)
 
-  const fetchUserDetails = async()=>{
-      const dataResponse = await fetch(SummaryApi.current_user.url,{
-        method : SummaryApi.current_user.method,
-        credentials : 'include'
-      })
-
-      const dataApi = await dataResponse.json();
-
-      if(dataApi.success){
-        dispatch(setUserDetails(dataApi.data));
-      }
-  }
+ 
 
   const fetchUserDetails = async () => {
   try {
